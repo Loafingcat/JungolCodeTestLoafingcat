64c8df5f7ab0a402f3b234fd6c47817b73a990ed
Junho Byun <bjunho0130@gmail.com>
2022-01-19 22:19:32 +0900

    122번 문제 메소드로 변경

--- src/JH/codetest/Jungol122.java
+++ src/JH/codetest/Jungol122.java
@@ -3,13 +3,21 @@ package JH.codetest;
 import java.util.Scanner;
 public class Jungol122 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int x;
         Scanner sc = new Scanner(System.in);
-        x=sc.nextInt();
-        if(x%400==0||(x%4==0&&x%100!=0))System.out.println("leap year");
-        else System.out.println("common year");
+        x = sc.nextInt();
+        if (isLeapYear(x)) {
+            System.out.println("leap year");
+        } else {
+            System.out.println("common year");
 
+        }
+    }
+
+    private static boolean isLeapYear(int year) {
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+        
     }
 
 }
