d93de143df37e1ad943135a1b18cae17a03936fc
Junho Byun <bjunho0130@gmail.com>
2022-01-31 04:28:39 +0900

    정올 159번 수정

--- src/JH/codetest/Jungol159.java
+++ src/JH/codetest/Jungol159.java
@@ -6,16 +6,14 @@ public class Jungol159 {
     //입력받아 높은 점수부터 차례로 출력하는 프로그램을 작성하시오.
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("20이하의 정수 입력: ");
+        System.out.print("20이하의 정수를 입력하세요: ");
         //변수 머머머 필요하냐
         //20이하의 정수 n은 20짜리 배열 선언 해주고
         //입력받은 정수의 개수가 점수 배열의 개수
         //그리고 내림차순으로 정리
         //20이하의 정수 n을 입력받아 출력
         int[] arr = new int[20];
-        int num = 0;
-        int score = 0;
-        int temp = 0;
+        int num, score, temp = 0;
 
         num = sc.nextInt();
         if (num > 20) {
@@ -28,7 +26,7 @@ public class Jungol159 {
         }
         //높은 점수부터 차례로 출력
         for (int i = 0; i < num; i++) {
-            for (int j = i; j < num; j++) {
+            for (int j = i+1; j < num; j++) {
                 if (arr[i] < arr[j]) {
                     temp = arr[i];
                     arr[i] = arr[j];
