210e2f6bafe6a4d914100a68a9043f3508d9a049
Junho Byun <bjunho0130@gmail.com>
2022-02-07 21:51:29 +0900

    jungol159 코드 수정

--- src/JH/codetest/Jungol159.java
+++ src/JH/codetest/Jungol159.java
@@ -1,41 +1,30 @@
 package JH.codetest;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Jungol159 {
-    //20 이하의 정수 n을 입력받고 n명의 점수를 il.Scanner;
-    //입력받아 높은 점수부터 차례로 출력하는 프로그램을 작성하시오.
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("20이하의 정수를 입력하세요: ");
-        //변수 머머머 필요하냐
-        //20이하의 정수 n은 20짜리 배열 선언 해주고
-        //입력받은 정수의 개수가 점수 배열의 개수
-        //그리고 내림차순으로 정리
-        //20이하의 정수 n을 입력받아 출력
-        int[] arr = new int[20];
-        int num, score, temp = 0;
 
+        int num, score = 0;
         num = sc.nextInt();
         if (num > 20) {
-            System.out.println("20이하의 정수만 입력하세요");
+            System.out.println("20이하의 정수만 입력하세요, 다시 입력하세요.");
+            System.exit(0);
         }
+        Integer[] arr = new Integer[num];
         System.out.print(num + "개의 성적을 입력하세요: ");
         for (int i = 0; i < num; i++) {
             score = sc.nextInt();
             arr[i] = score;
         }
-        //높은 점수부터 차례로 출력
-        for (int i = 0; i < num; i++) {
-            for (int j = i+1; j < num; j++) {
-                if (arr[i] < arr[j]) {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
-        for (int i = 0; i < num; i++) {
-            System.out.println(arr[i]);
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (int i : arr) {
+            System.out.println(i);
         }
     }
 }
