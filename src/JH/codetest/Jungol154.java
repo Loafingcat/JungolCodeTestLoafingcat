fb9ceeda92b3f995cd887bd669155c74e6f8804a
Junho Byun <bjunho0130@gmail.com>
2022-02-15 12:34:05 +0900

    정올 154번 수정완료

--- src/JH/codetest/Jungol154.java
+++ src/JH/codetest/Jungol154.java
@@ -9,6 +9,7 @@ public class Jungol154 {
 
         Scanner sc = new Scanner(System.in);
         double[] weight = new double[5];
+        Arrays.sort(weight);
         double avg = 0;
 
         for (int i = 0; i < 5; i++) {
@@ -20,9 +21,6 @@ public class Jungol154 {
 
         System.out.printf("%.1f%n", avg);// %.소수점자리수f 가 반올림한 결과를 보여준다. String.Format을 이용한 방법
         // printf()를 줄바꿈 하고 싶으면 큰따옴표 안에 \n이나 %n을 넣으면
-        for(double d : weight){
-            System.out.println(weight[0]);
-            System.out.println(weight[weight.length-1]);
-        }
+        System.out.print("가장 가벼운 사람 :"+weight[0]+" "+"가장 무거운 사람 : "+weight[weight.length-1]);
     }
 }
