f985e0f6e475cfed24caaf00a7d7bea38c6de661
Junho Byun <bjunho0130@gmail.com>
2022-02-16 17:29:05 +0900

    정올 154번 리뉴얼 문제 해답 추가

--- src/JH/codetest/Jungol154.java
+++ src/JH/codetest/Jungol154.java
@@ -6,22 +6,24 @@ import java.util.Scanner;
 public class Jungol154 {
     public static void main(String[] args) {
 
-
         Scanner sc = new Scanner(System.in);
         double[] weight = new double[5];
         Arrays.sort(weight);
-        double avg = 0;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < weight.length; i++) {
             weight[i] = sc.nextDouble();
-            // weight[] 배열의 길이에 상관 없이 평균을 구하는 메소드를 만들고, 이걸 갖다 써서 구현 해 보시오.
-            avg += weight[i];
+        }
+        double avg = avg(weight);
+        // weight[] 배열의 길이에 상관 없이 평균을 구하는 메소드를 만들고, 이걸 갖다 써서 구현 해 보시오.
+        System.out.printf("%.1f%n", avg);
+        System.out.print("가장 가벼운 사람 :" + weight[0] + " " + "가장 무거운 사람 : " + weight[weight.length - 1]);
         }
 
-        avg /= 5;
-
-        System.out.printf("%.1f%n", avg);// %.소수점자리수f 가 반올림한 결과를 보여준다. String.Format을 이용한 방법
-        // printf()를 줄바꿈 하고 싶으면 큰따옴표 안에 \n이나 %n을 넣으면
-        System.out.print("가장 가벼운 사람 :"+weight[0]+" "+"가장 무거운 사람 : "+weight[weight.length-1]);
+    private static double avg(double[] weight) {
+        double sum = 0;
+        for (double number : weight) {
+            sum = sum + number;
+        }
+        return sum/weight.length;
     }
 }
