9d92994bb74abed2eacbdb1a4d5954530d910dab
Junho Byun <bjunho0130@gmail.com>
2022-02-17 20:20:31 +0900

    정올 154 수정사정

--- src/JH/codetest/Jungol154.java
+++ src/JH/codetest/Jungol154.java
@@ -7,23 +7,23 @@ public class Jungol154 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        double[] weight = new double[5];
-        Arrays.sort(weight);
+        double[] weights = new double[5];
+        Arrays.sort(weights);
 
-        for (int i = 0; i < weight.length; i++) {
-            weight[i] = sc.nextDouble();
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = sc.nextDouble();
         }
-        double avg = avg(weight);
-        // weight[] 배열의 길이에 상관 없이 평균을 구하는 메소드를 만들고, 이걸 갖다 써서 구현 해 보시오.
+        double avg = avg(weights);
+        // weights[] 배열의 길이에 상관 없이 평균을 구하는 메소드를 만들고, 이걸 갖다 써서 구현 해 보시오.
         System.out.printf("%.1f%n", avg);
-        System.out.print("가장 가벼운 사람 :" + weight[0] + " " + "가장 무거운 사람 : " + weight[weight.length - 1]);
-        }
+        System.out.print("가장 가벼운 사람 :" + weights[0] + " " + "가장 무거운 사람 : " + weights[weights.length - 1]);
+    }
 
-    private static double avg(double[] weight) {
+    private static double avg(double[] weights) {
         double sum = 0;
-        for (double number : weight) {
-            sum = sum + number;
+        for (double number : weights) {
+            sum += number;
         }
-        return sum/weight.length;
+        return sum/weights.length;
     }
 }
