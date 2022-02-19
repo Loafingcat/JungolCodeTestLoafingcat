9be027069afb68b96a1acfd7a9c2a765df7f689a
Junho Byun <bjunho0130@gmail.com>
2022-02-19 23:21:03 +0900

    정올 154 min(), max() 메소드로 빼서 수정

--- src/JH/codetest/Jungol154.java
+++ src/JH/codetest/Jungol154.java
@@ -8,15 +8,29 @@ public class Jungol154 {
 
         Scanner sc = new Scanner(System.in);
         double[] weights = new double[5];
-        Arrays.sort(weights);
 
         for (int i = 0; i < weights.length; i++) {
             weights[i] = sc.nextDouble();
         }
         double avg = avg(weights);
+        double min = min(weights);
+        double max = max(weights);
+
         // weights[] 배열의 길이에 상관 없이 평균을 구하는 메소드를 만들고, 이걸 갖다 써서 구현 해 보시오.
         System.out.printf("%.1f%n", avg);
-        System.out.print("가장 가벼운 사람 :" + weights[0] + " " + "가장 무거운 사람 : " + weights[weights.length - 1]);
+        System.out.print("가장 가벼운 사람 :" + min + " " + "가장 무거운 사람 : " + max);
+    }
+
+    private static double min(double[] weights) {
+        double[] copy = Arrays.copyOf(weights, weights.length);
+        Arrays.sort(copy);
+        return copy[0];
+    }
+
+    private static double max(double[] weights) {
+        double[] copy = Arrays.copyOf(weights, weights.length);
+        Arrays.sort(copy);
+        return copy[weights.length - 1];
     }
 
     private static double avg(double[] weights) {
