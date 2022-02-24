9156dff71a41d3260957842cf842fdbd0ae86721
Junho Byun <bjunho0130@gmail.com>
2022-02-24 16:37:15 +0900

    Jungol 191 완벽한 정답

--- src/JH/codetest/Jungol191.java
+++ src/JH/codetest/Jungol191.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 
 public class Jungol191 {
     public static void main(String[] args) {
-        //50개를 넘지 않고 길이가 100이하인 단어를 입력받기
-        //0을 입력 받으면 입력을 종료하고 그 때까지 입력받은 단어 개수 출력
-        //홀수 번째 입력받은 단어를 한줄에 1개씩 출력
+
         Scanner sc = new Scanner(System.in);
         String str[] = new String[50];
         String counts;
@@ -16,6 +14,10 @@ public class Jungol191 {
             counts = sc.next();
             if(counts.equals("0"))
                 break;
+            else if (counts.length() > 100) {
+                System.out.print("100글자 미만의 단어만 입력하세요.");
+                System.exit(0);
+            }
             str[i] = counts;
         }
         System.out.println("입력된 단어의 개수 : " + i);
