13e39d8477474989729725b3be22103ec08fce51
Junho Byun <bjunho0130@gmail.com>
2022-04-20 21:11:45 +0900

    jungol207 문제

---
+++ src/JH/codetest/Jungol207.java
@@ -0,0 +1,58 @@
+package JH.codetest;
+
+import java.util.Scanner;
+
+public class Jungol207 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char op; //operater 연산자
+        int num1;
+        int num2;
+        double result;
+
+        System.out.println("+, -, *, /, % 중 하나를 고르세요");
+        op = sc.next().charAt(0);
+
+        switch(op) {
+            case '+':
+                System.out.println("숫자 두 개 입력");
+                num1 = sc.nextInt();
+                num2 = sc.nextInt();
+                result = num1 + num2;
+                System.out.println("두 수를" + op + "한 값은" + result + "입니다.");
+                break;
+            case '-':
+                System.out.println("숫자 두 개 입력");
+                num1 = sc.nextInt();
+                num2 = sc.nextInt();
+                result = num1 - num2;
+                System.out.println("두 수를" + op + "한 값은" + result + "입니다.");
+                break;
+            case '*':
+                System.out.println("숫자 두 개 입력");
+                num1 = sc.nextInt();
+                num2 = sc.nextInt();
+                result = num1 * num2;
+                System.out.println("두 수를" + op + "한 값은" + result + "입니다.");
+                break;
+            case '/':
+                System.out.println("숫자 두 개 입력");
+                num1 = sc.nextInt();
+                num2 = sc.nextInt();
+                result = num1 / num2;
+                System.out.println("두 수를" + op + "한 값은" + result + "입니다.");
+                break;
+            case '%':
+                System.out.println("숫자 두 개 입력");
+                num1 = sc.nextInt();
+                num2 = sc.nextInt();
+                result = num1 % num2;
+                System.out.println("두 수를" + op + "한 값은" + result + "입니다.");
+                break;
+            default:
+                System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
+        }
+    }
+}
