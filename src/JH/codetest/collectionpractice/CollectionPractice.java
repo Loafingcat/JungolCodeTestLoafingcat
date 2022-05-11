f05d21a28aee9402f6f72f09d7114784a42e1540
Junho Byun <bjunho0130@gmail.com>
2022-05-11 22:05:27 +0900

    arraylist

---
+++ src/JH/codetest/collectionpractice/CollectionPractice.java
@@ -0,0 +1,19 @@
+package JH.codetest.collectionpractice;
+
+import java.util.ArrayList;
+
+public class CollectionPractice {
+
+    public static void main(String[] args) {
+
+        //콜렉션 (여래 종류의 자료 구조 List, Map, Set, ....를 담은 JDK 패키지-도구-)
+
+        //배열 기능의 한계
+        int[] nums = new int[20]; // 배열 길이를 처음 선언할 때 부터 정해야 한다.
+                                  // jungol140 문제 처럼 '입력받는 데이터의 양에 따라 길이가 정해지는 배열'이 있으면 편할텐데?
+
+        // List
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+    }
+}
---
+++ src/JH/codetest/collectionpractice/CollectionPractice2.java
@@ -0,0 +1,17 @@
+package JH.codetest.collectionpractice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CollectionPractice2 {
+    public static void main(String[] args) {
+        // 숫자를 입력받고, 0을 입력받으면 입력 그만 받고
+        // 입력받은 숫자를 모두 출력하시오.
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.print("숫자를 입력하세요: ");
+
+
+        }
+    }
+}
