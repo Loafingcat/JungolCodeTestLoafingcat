c61723507ce2be2dad79fe78d0f7e23ed8c7313b
Junho Byun <bjunho0130@gmail.com>
2022-05-11 22:29:06 +0900

    콜렉션(arraylist)이 무엇인가?

--- src/JH/codetest/collectionpractice/CollectionPractice2.java
+++ src/JH/codetest/collectionpractice/CollectionPractice2.java
@@ -1,11 +1,25 @@
 package JH.codetest.collectionpractice;
 
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CollectionPractice2 {
     public static void main(String[] args) {
         // collection 연습문제
         // 숫자를 입력받고, 0을 입력받으면 입력 그만 받고
         // 입력받은 숫자를 모두 출력하시오.
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
 
+        System.out.println("숫자를 입력하세요: ");
+        int num = sc.nextInt();
+        while (num != 0) {
+            numbers.add(num);
+            num = sc.nextInt();
+        }
+        for (int i : numbers) {
+            System.out.println(i);
+        }
     }
 }
