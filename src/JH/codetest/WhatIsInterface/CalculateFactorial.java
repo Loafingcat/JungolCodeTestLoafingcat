6d43406c124939289124c970d13d93334e9e0427
jaehun-jeong <keepthefaith00@gmail.com>
2022-07-12 20:07:37 +0900

    후속 숙제 주석 추가

--- src/JH/codetest/WhatIsInterface/CalculateFactorial.java
+++ src/JH/codetest/WhatIsInterface/CalculateFactorial.java
@@ -1,6 +1,6 @@
 package JH.codetest.WhatIsInterface;
 
-public interface CalculateFactorial {
+public interface CalculateFactorial { // 인터페이스 이름 : ~able 접미어 등을 써서 만듬.
 
     void calculate();
 }
--- src/JH/codetest/WhatIsInterface/Factorial.java
+++ src/JH/codetest/WhatIsInterface/Factorial.java
@@ -3,9 +3,13 @@ package JH.codetest.WhatIsInterface;
 
 public class Factorial {
 
+    // 보완숙제 :
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator(); // Caculator : 계산기, 뭘 계산하는지? 클래스 이름 불명확하다.
 
+        // 매개변수가 들어가면 좋을듯 : 매개변수 역할은 n부터 factorial 연산 할건지?
+        // caculate()는 사용자의 입력도 받고있고, 계산도 수행하고, console로 출력도 수행하다.
+        // 각각 분리해서 구현 해보삼.
         calculator.calculate();
     }
 }
